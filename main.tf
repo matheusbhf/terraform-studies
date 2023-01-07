ae523c70666a746650417b9734d00185b1fef102
Matheus <matheusbhf@gmail.com>
2023-01-07 20:21:19 -0300

    FIRST commit

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-06878d265978313ca"
+  instance_type = "t2.micro"
+  key_name = "matheus-useast1"
+
+  tags = {
+    Name = "PrimeiraInstancia"
+  }
+}
