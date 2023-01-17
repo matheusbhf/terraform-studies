885cb1d2266dd122fb1fc3a6595a16ceb0f2c505
Matheus <matheusbhf@gmail.com>
2023-01-17 12:16:42 -0300

    enviroment created

--- main.tf
+++ main.tf
@@ -17,8 +17,7 @@ resource "aws_instance" "app_server" {
   ami           = "ami-06878d265978313ca"
   instance_type = "t2.micro"
   key_name = "matheus-useast1"
-
   tags = {
-    Name = "PrimeiraInstancia"
+    Name = "terraform-ansible-python"
   }
 }
